b99012fc4135c3634306a4372625eb299e4d08c1
hayakawa01 <hay.kaw.you.6820@gmail.com>
2023-07-25 08:12:47 +0900

    環境構築 devserver起動確認

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import World from "./world";
+const root = document.getElementById("root");
+const world = new World("Hello hogehoge!!!");
+world.sayHello(root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,34 @@
+const path = require("path");
 // webpackの設定ファイル
 module.exports = {
+  // モジュールをバンドルする際の
   // エントリーポイント（起点）となる変数の命名と参照ファイルの設定
   entry: {
     bundle: "./src/index.ts",
+  },
+  // 出力先のパス、今回は最終的にts→js似直したdistファイルのあるdistディレクトリを参照する
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    open: true,
+    hot: true,
+    // contentBase: path.join(__dirname, "dist"),  versionが新しくなったので書き直す必要がある
+    // https://zenn.dev/kaito_takase/articles/abdb62bf8e352a
+  },
+  module: {
+    rules: [
+      {
+        loader: "ts-loader",
+        test: /\.ts$/, //   \. → ドットエスケープ 、$末尾指定
+      },
+    ],
   },
 };
